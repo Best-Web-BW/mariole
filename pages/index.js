0aee11bfc07799a6cb0b9a5d1fd124cce2d01495
OcelotJungle <ocelotjungle@yandex.ru>
2021-03-16 13:06:31 +0500

    Some changes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
+import ProductCard from "../components/ProductCard";
+import blocks from "../scss/blocks.module.scss";
+import styles from "./index.module.scss";
 import Head from "next/head";
 import Link from "next/link";
 import cn from "classnames";
-import { useState } from "react";
-import blocks from "../scss/blocks.module.scss";
-import styles from "./index.module.scss";
-import Product from "../components/product_card";
 
 export default function Index() {
-
     return (<>
         <Head>
             <title>Mariole</title>
@@ -28,10 +26,9 @@ export default function Index() {
             <meta name="keywords" content="mariole, mariole, mariole" />
         </Head>
         <div className={blocks.content_body}>
-            
             <div className={cn(blocks.main_block, styles.first_block)}>
-                <img src="/images/blocks/FullSizeRender1.jpg" alt="" className={blocks.desktop}/>
-                <img src="/images/blocks/mario_le-1817.jpg" alt="" className={blocks.mobile} width="100%"/>
+                <img className={blocks.desktop} src="/images/blocks/FullSizeRender1.jpg" alt="" />
+                <img className={blocks.mobile} src="/images/blocks/mario_le-1817.jpg" alt="" width="100%" />
                 <Link href="/shop">
                     <a className={blocks.absolute_link}>
                         <button>НАЧАТЬ ШОПИНГ</button>
@@ -42,25 +39,32 @@ export default function Index() {
                 <div className={styles.about_area}>
                     <div className={cn(blocks.row, styles.row)}>
                         <p>
-                            Ремесло, история которого в России насчитывает более 250 лет, с помощью современных технологий, обретает новую жизнь.
+                            Ремесло, история которого в России насчитывает более 250 лет, 
+                            с помощью современных технологий, обретает новую жизнь.
                         </p>
                     </div>
                     <div className={cn(blocks.column, styles.column)}>
-                        <img src="/images/blocks/mario_le-1684.jpg" alt=""/>
+                        <img src="/images/blocks/mario_le-1684.jpg" alt="" />
                     </div>
                     <div className={cn(blocks.column, styles.column)}>
                         <p>
-                            Mario’le - это эксклюзивная вязаная одежда и аксессуары ручной работы из пуха знаменитых Оренбургских коз.
-                            <br/>
-                            <br/>
-                            Секрет Mario’le - уникальные свойства используемого сырья.На своих фермах в Оренбургской области мы разводим коз реликтовой породы, пух которых удивительно тонок и эластичен.
-                            <br/>
-                            <br/>
-                            Именно его неповторимые  свойства, позволяют нам изготавливать превосходную пряжу ручного прядения,
+                            Mario’le - это эксклюзивная вязаная одежда и аксессуары 
+                            ручной работы из пуха знаменитых Оренбургских коз.
+                            <br />
+                            <br />
+                            Секрет Mario’le - уникальные свойства используемого сырья. 
+                            На своих фермах в Оренбургской области мы разводим коз 
+                            реликтовой породы, пух которых удивительно тонок и эластичен.
+                            <br />
+                            <br />
+                            Именно его неповторимые  свойства, позволяют нам 
+                            изготавливать превосходную пряжу ручного прядения, 
                             лёгкую и невесомую  словно кружево.
-                            <br/>
-                            <br/>⠀
-                            Опираясь на вековые традиции и следуя тенденциям современного дизайна, мы гарантируем исключительно высокое качество наших изделий.
+                            <br />
+                            <br />⠀
+                            Опираясь на вековые традиции и следуя тенденциям 
+                            современного дизайна, мы гарантируем исключительно 
+                            высокое качество наших изделий.
                         </p>
                     </div>
                 </div>
@@ -74,10 +78,10 @@ export default function Index() {
                 </div>
                 <div className={styles.products_block}>
                     <div className={cn(blocks.row, styles.product_row)}>
-                        <Product/>
-                        <Product/>
-                        <Product/>
-                        <Product/>
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
                     </div>
                 </div>
             </div>
@@ -85,8 +89,8 @@ export default function Index() {
                 <Link href="/shop">
                     <a className={styles.photo_link}>
                         <p className={styles.photo_link_title}>ТРИКОТАЖ</p>
-                        <img src="/images/blocks/FullSizeRender.jpg" alt="" className={styles.desktop}/>
-                        <img src="/images/blocks/mario_le-1684.jpg" alt="" className={styles.mobile}/>
+                        <img className={styles.desktop} src="/images/blocks/FullSizeRender.jpg" alt="" />
+                        <img className={styles.mobile} src="/images/blocks/mario_le-1684.jpg" alt="" />
                     </a>
                 </Link>
             </div>
@@ -99,10 +103,10 @@ export default function Index() {
                 </div>
                 <div className={styles.products_block}>
                     <div className={cn(blocks.row, styles.product_row)}>
-                        <Product/>
-                        <Product/>
-                        <Product/>
-                        <Product/>
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
                     </div>
                 </div>
             </div>
@@ -113,12 +117,12 @@ export default function Index() {
                 <div className={cn(blocks.row, styles.press)}>
                     <Link href="#">
                         <a className={styles.press_elem}>
-                            <img src="/images/press/aeroflot.png" alt=""/>
+                            <img src="/images/press/aeroflot.png" alt="" />
                         </a>
                     </Link>
                     <Link href="#">
                         <a className={styles.press_elem}>
-                            <img src="/images/press/elle_decoration.png" alt=""/>
+                            <img src="/images/press/elle_decoration.png" alt="" />
                         </a>
                     </Link>
                 </div>
