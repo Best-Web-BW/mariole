48a0398e97fc5db0158bf205b30acf197dd253c9
OcelotJungle <ocelotjungle@yandex.ru>
2021-03-20 08:31:03 +0500

    Update media.js

--- pages/media.js
+++ pages/media.js
@@ -8,26 +8,23 @@ export default function Media() {
         <div className={cn(blocks.content_block, styles.page)}>
             <h2 className={styles.page_title}>О НАС ПИШУТ</h2>
             <div className={styles.row}>
-                <div className={styles.elem}>
-                    <div className={styles.elem_row_1}>
-                        <img src="/images/press/page_1.jpg" alt="" />
-                    </div>
-                    <div className={styles.elem_row_2}>
-                        <Link href="#">
-                            <a className={styles.read}>Читать в источнике</a>
-                        </Link>
-                    </div>
-                </div>
-                <div className={styles.elem}>
-                    <div className={styles.elem_row_1}>
-                        <img src="/images/press/_file5afd3e200b21b.jpg" alt="" />
-                    </div>
-                    <div className={styles.elem_row_2}>
-                        <Link href="#">
-                            <a className={styles.read}>Читать в источнике</a>
-                        </Link>
-                    </div>
-                </div>
+                <ContentBlock image="/images/press/page_1.jpg" link="#" />
+                <ContentBlock image="/images/press/_file5afd3e200b21b.jpg" link="#" />
+            </div>
+        </div>
+    );
+}
+
+function ContentBlock({ image, link }) {
+    return (
+        <div className={styles.elem}>
+            <div className={styles.elem_row_1}>
+                <img src={image} alt="" />
+            </div>
+            <div className={styles.elem_row_2}>
+                <Link href={link}>
+                    <a className={styles.read}>Читать в источнике</a>
+                </Link>
             </div>
         </div>
     );
