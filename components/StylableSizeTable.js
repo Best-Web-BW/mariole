730a2c2f148ef95ec199ff9b86389025cf17c851
OcelotJungle <ocelotjungle@yandex.ru>
2021-03-25 16:11:59 +0500

    Moved size table to the separated component

---
+++ components/StylableSizeTable.js
@@ -0,0 +1,52 @@
+export default function StylableSizeTable({ styles }) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <td className={styles.row_title}>Mario'le</td>
+                    <td className={styles.col_title}>XS</td>
+                    <td className={styles.col_title}>S</td>
+                    <td className={styles.col_title}>M</td>
+                    <td className={styles.col_title}>L</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className={styles.row_title}>Объём груди (см)</td>
+                    <td>84</td>
+                    <td>88</td>
+                    <td>92</td>
+                    <td>96</td>
+                </tr>
+                <tr>
+                    <td className={styles.row_title}>Объём талии (см)</td>
+                    <td>64</td>
+                    <td>68</td>
+                    <td>72</td>
+                    <td>76</td>
+                </tr>
+                <tr>
+                    <td className={styles.row_title}>Объём бёдер (см)</td>
+                    <td>88</td>
+                    <td>92</td>
+                    <td>96</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <td className={styles.row_title}>Италия</td>
+                    <td>38</td>
+                    <td>40</td>
+                    <td>42</td>
+                    <td>44</td>
+                </tr>
+                <tr>
+                    <td className={styles.row_title}>Франция</td>
+                    <td>36</td>
+                    <td>38</td>
+                    <td>40</td>
+                    <td>42</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
--- pages/size-table.js
+++ pages/size-table.js
@@ -1,3 +1,4 @@
+import StylableSizeTable from "../components/StylableSizeTable";
 import { content_block } from "../scss/blocks.module.scss";
 import styles from "./size-table.module.scss";
 import cn from "classnames";
@@ -7,54 +8,7 @@ export default function SizeTable() {
         <div className={cn(content_block, styles.page)}>
             <h2 className={styles.page_title}>Таблица размеров</h2>
             <div className={styles.table_wrapper}>
-                <table>
-                    <thead>
-                        <tr>
-                            <td className={styles.row_title}>Mario'le</td>
-                            <td className={styles.col_title}>XS</td>
-                            <td className={styles.col_title}>S</td>
-                            <td className={styles.col_title}>M</td>
-                            <td className={styles.col_title}>L</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className={styles.row_title}>Объём груди (см)</td>
-                            <td>84</td>
-                            <td>88</td>
-                            <td>92</td>
-                            <td>96</td>
-                        </tr>
-                        <tr>
-                            <td className={styles.row_title}>Объём талии (см)</td>
-                            <td>64</td>
-                            <td>68</td>
-                            <td>72</td>
-                            <td>76</td>
-                        </tr>
-                        <tr>
-                            <td className={styles.row_title}>Объём бёдер (см)</td>
-                            <td>88</td>
-                            <td>92</td>
-                            <td>96</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td className={styles.row_title}>Италия</td>
-                            <td>38</td>
-                            <td>40</td>
-                            <td>42</td>
-                            <td>44</td>
-                        </tr>
-                        <tr>
-                            <td className={styles.row_title}>Франция</td>
-                            <td>36</td>
-                            <td>38</td>
-                            <td>40</td>
-                            <td>42</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <StylableSizeTable styles={styles} />
             </div>
         </div>
     );
