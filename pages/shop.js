6c3d0c22609dd8e0599edca49bc3a963e98569e4
OcelotJungle <ocelotjungle@yandex.ru>
2021-03-25 16:17:01 +0500

    Major changes
    Added shop filtering, SSR.

--- pages/shop.js
+++ pages/shop.js
@@ -1,14 +1,16 @@
+import divideArrayToSubarrays from "../utils/arrays/divideToSubarrays";
 import { getMenuItem } from "../components/MenuItemGenerator";
+import admin from "../scss/adminButtons.module.scss";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import blocks from "../scss/blocks.module.scss";
-import admin from "../scss/adminButtons.module.scss";
 import styles from "./shop.module.scss";
-import lorem from "../utils/lorem";
 import Select from "react-select";
-import { useState } from "react";
-import Head from "next/head";
 import Link from "next/link";
 import cn from "classnames";
+import { _get } from "./api/products";
+import { useRouter } from "next/router";
+import { joinQuery } from "../utils/common/network";
 
 const sortingOptions = [
     { value: "price_0-1", label: "Цена по возрастанию" },
@@ -45,8 +47,73 @@ const selectStyles = {
     }
 }
 
-export default function Shop() {
+function parseQuery({ category, subcategory, limited, bestseller, sizes, search }) {
+    return {
+        category: uri.decode.string(category),
+        subcategory: uri.decode.string(subcategory),
+        limited: uri.decode.boolean(limited),
+        bestseller: uri.decode.boolean(bestseller),
+        sizes: uri.decode.array(sizes),
+        search: uri.decode.string(search)
+    };
+}
+
+const uri = {
+    decode: {
+        boolean: raw => Boolean(raw),
+        string: raw => raw ? decodeURIComponent(raw) : undefined,
+        array: raw => raw ? decodeURIComponent(raw).split(",") : []
+    },
+    encode: {
+        boolean: raw => raw ? +raw : undefined,
+        string: raw => raw ? encodeURIComponent(raw) : undefined,
+        array: raw => raw.length ? encodeURIComponent(raw.toString()) : undefined
+    }
+}
+
+export default function Shop({ defaultProducts }) {
     const [mobileMenuOpened, setMobileMenuOpened] = useState(false);
+
+    const [products, setProducts] = useState(defaultProducts);
+    const router = useRouter();
+    const [filter, setFilter] = useState(parseQuery(router.query));
+    // useEffect(() => console.log({ filter }), [filter]);
+
+    useEffect(async () => {
+        const response = await fetch(`/api/products?${joinQuery(filter)}`);
+        setProducts(await response.json());
+    }, [filter]);
+    
+    useEffect(() => setFilter(parseQuery(router.query)), [router.query]);
+
+    const updateQuery = useCallback(entries => {
+        const query = { ...router.query };
+        for(const [name, value] of entries) value ? query[name] = value : delete query[name];
+        router.push({ pathname: "/shop", query }, undefined, { shallow: true });
+    }, [router.query]);
+
+    const toggle = useMemo(() => ({
+        category: name => {
+            updateQuery([
+                ["category", filter.category !== name ? uri.encode.string(name) : undefined],
+                ["subcategory", undefined]
+            ]);
+        },
+        subcategory: (categoryName, name) => {
+            updateQuery([
+                ["category", categoryName],
+                ["subcategory", filter.subcategory !== name ? uri.encode.string(name) : undefined]
+            ]);
+        },
+        limited: state => updateQuery([["limited", uri.encode.boolean(state)]]),
+        bestseller: state => updateQuery([["bestseller", uri.encode.boolean(state)]]),
+        size: size => {
+            const newState = !filter.sizes.includes(size);
+            const sizes = filter.sizes.filter(entry => entry !== size);
+            updateQuery([["sizes", uri.encode.array(newState ? [...sizes, size] : sizes)]]);
+        },
+        search: text => updateQuery([["search", uri.encode.string(text)]])
+    }), [updateQuery, filter]);
 
     return (<>
         <div className={cn(blocks.main_block, styles.first_block)}>
@@ -63,36 +130,12 @@ export default function Shop() {
                 </div>
                 <div className={styles.menu_container}>
                     <ul>
-                        <MenuFilterFunction title="КАТЕГОРИЯ">
-                            <SubmenuFilterFunction href="/shop" title="Трикотаж">
-                                <MenuItem href="/shop" title="Костюмы" />
-                                <MenuItem href="/shop" title="Водолазки" />
-                                <MenuItem href="/shop" title="Кардиганы" />
-                            </SubmenuFilterFunction>
-                            <SubmenuFilterFunction href="/shop" title="Аксессуары">
-                                <MenuItem href="/shop" title="Варежки" />
-                                <MenuItem href="/shop" title="Перчатки" />
-                                <MenuItem href="/shop" title="Носки" />
-                                <MenuItem href="/shop" title="Косынки" />
-                                <MenuItem href="/shop" title="Платки" />
-                                <MenuItem href="/shop" title="Палантины" />
-                                <MenuItem href="/shop" title="Головные уборы" />
-                            </SubmenuFilterFunction>
-                            <SubmenuFilterFunction href="/shop" title="Аксессуары">
-                                <MenuItem href="/shop" title="Колье" />
-                                <MenuItem href="/shop" title="Серьги" />
-                                <MenuItem href="/shop" title="Браслеты" />
-                                <MenuItem href="/shop" title="Броши" />
-                            </SubmenuFilterFunction>
-                            <MenuItem href="/shop" title="Лимитированная коллекция" />
-                            <MenuItem href="/shop" title="Бестселлеры" />
-                        </MenuFilterFunction>
-                        <MenuFilterFunction title="РАЗМЕР">
-                            <SizeItem active title="L" />
-                            <SizeItem title="M" />
-                            <SizeItem title="S" />
-                            <SizeItem title="XS" />
-                        </MenuFilterFunction>
+                        <CategoryMenu toggle={toggle} filter={filter} />
+                        <SizeMenu
+                            existingSizes={["L", "M", "S", "XS"]}
+                            activeSizes={filter.sizes}
+                            toggle={toggle.size}
+                        />
                     </ul>
                 </div>
             </div>  
@@ -114,64 +157,157 @@ export default function Shop() {
                         />
                     </div>
                 </div>
-                <div className={styles.row}>
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                </div>
-                <div className={styles.row}>
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                </div>
+                <ProductList products={products} />
             </div>
         </div>
     </>);
 }
 
-const MenuItem = getMenuItem(styles.menu_elem);
-
-function SizeItem({ active, title }) {
-    return (
-        <li>
-            <Link href="#">
-                <a className={cn(styles.menu_elem, styles.size, { [styles.active]: active })}>
-                    <div className={styles.size_elem} />
-                    { title }
-                </a>
-            </Link>
-        </li>
-    );
-}
-
-function FilterFunction({ children, content }) {
+function CategoryMenu({ toggle, filter }) {
     const [expanded, setExpanded] = useState(false);
 
     return (
         <li>
             <div className={styles.menu_elem} onClick={() => setExpanded(prev => !prev)}>
                 <span className={cn(styles.submenu_icn, { [styles.active]: expanded })} />
-                { children }
+                КАТЕГОРИЯ
             </div>
             <div className={cn(styles.submenu_container, { [styles.opened]: expanded })}>
-                <ul>{ content }</ul>
+                <ul>
+                    <CategoryFilter
+                        toggle={toggle} name="knitwear" title="Трикотаж"
+                        subcategories={[
+                            ["suit", "Костюмы"],
+                            ["turtleneck", "Водолазки"],
+                            ["cardigans", "Кардиганы"]
+                        ]}
+                    />
+                    <CategoryFilter
+                        toggle={toggle} name="accessories" title="Аксессуары"
+                        subcategories={[
+                            ["mittens", "Варежки"],
+                            ["gloves", "Перчатки"],
+                            ["socks", "Носки"],
+                            ["headscarves", "Косынки"],
+                            ["shawls", "Платки"],
+                            ["stoles", "Палантины"],
+                            ["headwear", "Головные уборы"]
+                        ]}
+                    />
+                    <CategoryFilter
+                        toggle={toggle} name="jewelry" title="Украшения"
+                        subcategories={[
+                            ["necklaces", "Колье"],
+                            ["earrings", "Серьги"],
+                            ["bracelets", "Браслеты"],
+                            ["brooches", "Броши"]
+                        ]}
+                    />
+                    <LimitedFilter toggle={toggle.limited} active={filter.limited} />
+                    <BestsellerFilter toggle={toggle.bestseller} active={filter.bestseller} />
+                </ul>
             </div>
         </li>
     );
 }
 
-function MenuFilterFunction({ title, children }) {
-    return <FilterFunction content={children}>{ title }</FilterFunction>;
+function CategoryFilter({ toggle, name, title, subcategories }) {
+    const [expanded, setExpanded] = useState(false);
+
+    return (
+        <li>
+            <span
+                className={cn(styles.submenu_icn, { [styles.active]: expanded })}
+                onClick={() => setExpanded(prev => !prev)}
+            />
+            <span className={styles.menu_elem} onClick={() => toggle.category(name)}>{ title }</span>
+            <div className={cn(styles.submenu_container, { [styles.opened]: expanded })}>
+                <ul>{
+                    subcategories.map(([_name, _title]) => (
+                        <SubcategoryFilter
+                            key={_name} name={_name} title={_title}
+                            toggle={() => {
+                                toggle.subcategory(name, _name);
+                            }}
+                        />
+                    ))
+                }</ul>
+            </div>
+        </li>
+    );
 }
 
-function SubmenuFilterFunction({ href, title, children }) {
+function SubcategoryFilter({ toggle, name, title }) {
     return (
-        <FilterFunction content={children}>
-            <Link href={href}>
-                <a className={styles.menu_elem}>{ title }</a>
-            </Link>
-        </FilterFunction>
+        <li onClick={() => toggle(name)}>
+            <span className={styles.menu_elem}>{ title }</span>
+        </li>
     );
+}
+
+function LimitedFilter({ toggle, active }) {
+    return (
+        <li onClick={() => toggle(!active)}>
+            <span className={styles.menu_elem}>Лимитированная коллекция</span>
+        </li>
+    );
+}
+
+function BestsellerFilter({ toggle, active }) {
+    return (
+        <li onClick={() => toggle(!active)}>
+            <span className={styles.menu_elem}>Бестселлеры</span>
+        </li>
+    );
+}
+
+function SizeItem({ toggle, active, name }) {
+    return (
+        <li onClick={toggle}>
+            <p className={cn(styles.menu_elem, styles.size, { [styles.active]: active })}>
+                <span className={styles.size_elem} />
+                { name }
+            </p>
+        </li>
+    );
+}
+
+function SizeMenu({ activeSizes, existingSizes, toggle }) {
+    const [expanded, setExpanded] = useState(false);
+
+    return (
+        <li>
+            <div className={styles.menu_elem} onClick={() => setExpanded(prev => !prev)}>
+                <span className={cn(styles.submenu_icn, { [styles.active]: expanded })} />
+                РАЗМЕР
+            </div>
+            <div className={cn(styles.submenu_container, { [styles.opened]: expanded })}>
+                <ul>{
+                    existingSizes.map(size => (
+                        <SizeItem
+                            key={size} name={size}
+                            toggle={() => toggle(size)}
+                            active={activeSizes.includes(size)}
+                        />
+                    ))
+                }</ul>
+            </div>
+        </li>
+    );
+}
+
+export async function getServerSideProps({ query }) {
+    const parsedQuery = { ...parseQuery(query), locale: "ru" };
+    if(!parsedQuery.sizes.length) parsedQuery.sizes = undefined;
+    const defaultProducts = _get(parsedQuery);
+    return { props: { defaultProducts } };
+}
+
+function ProductList({ products }) {
+    const divided = useMemo(() => divideArrayToSubarrays(products, 4), [products]);
+    return divided.map((sublist, index) => (
+        <div key={index} className={styles.row}>{
+            sublist.map((data, index) => <ProductCard key={index} data={data} />)
+        }</div>
+    ));
 }
