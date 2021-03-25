8b010ebedbee8934b28735af5ff2fa56dbd82f10
OcelotJungle <ocelotjungle@yandex.ru>
2021-03-25 16:10:53 +0500

    Added dynamic data in product card

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,18 +1,20 @@
+import formatPrice from "../utils/common/formatPrice";
 import styles from "./ProductCard.module.scss";
 import Link from "next/link";
+import cn from "classnames";
 
-export default function ProductCard({ id = 0 }) {
+export default function ProductCard({ data: { id, image, name, price, available } = {} }) {
 	return (
 		<div className={styles.product_card}>
-            <div className={styles.outofstock}>нет в наличии</div>
+            <div className={cn(styles.outofstock, { [styles.hidden]: available })}>нет в наличии</div>
             <Link href="/shop/[id]" as={`/shop/${id}`}>
                 <a className={styles.content}>
                     <div className={styles.image}>
-                        <img src="/images/products/mario_le-1817.jpg" alt="" />
+                        <img src={image} alt="" />
                     </div>
                     <div className={styles.data_wrapper}>
-                        <p className={styles.data}>Название продукта</p>
-                        <p className={styles.data}>32.000,00 &#8381;</p>
+                        <p className={styles.data}>{ name }</p>
+                        <p className={styles.data}>{ formatPrice(price) } &#8381;</p>
                     </div>
                 </a>
             </Link>
