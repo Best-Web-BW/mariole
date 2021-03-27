7fe438ba39e489485e26865b1e4578321024c505
OcelotJungle <ocelotjungle@yandex.ru>
2021-03-27 18:10:33 +0500

    Update store.js
    Removed @action where it's not action.
    Added recent functionality.

--- store.js
+++ store.js
@@ -12,31 +12,39 @@ class Store {
     }
 
     @observable favorite = [];
-    filterFavorite = id => this.favorite.filter(entry => entry !== id);
-
     @action setFavorite = favorite => {
         localStorage.favorite = JSON.stringify(favorite);
         this.favorite = favorite;
     };
-    @action addToFavorite = id => this.setFavorite([...this.filterFavorite(id), id]);
-    @action removeFromFavorite = id => this.setFavorite(this.filterFavorite(id));
-    @action toggleFavorite = id => {
-        this.favorite.includes(id) ? this.removeFromFavorite(id) : this.addToFavorite(id);
-    };
+    filterFavorite = id => this.favorite.filter(entry => entry !== id);
+    addToFavorite = id => this.setFavorite([...this.filterFavorite(id), id]);
+    removeFromFavorite = id => this.setFavorite(this.filterFavorite(id));
+    toggleFavorite = id => this.favorite.includes(id) ? this.removeFromFavorite(id) : this.addToFavorite(id);
 
     @observable cart = [];
-    filterCart = id => this.cart.filter(entry => entry !== id);
-
     @action setCart = cart => {
         localStorage.cart = JSON.stringify(cart);
         this.cart = cart;
     };
-    @action addToCart = id => this.setCart([...this.filterCart(id), id]);
-    @action removeFromCart = id => this.setCart(this.filterCart(id));   
+    filterCart = id => this.cart.filter(entry => entry.id !== id);
+    addToCart = (id, size, quantity) => this.setCart([...this.filterCart(id), { id, size, quantity }]);
+    removeFromCart = id => this.setCart(this.filterCart(id));
+    setCartQuantity = (id, quantity) => {
+        this.setCart(this.cart.map(entry => entry.id === id ? { ...entry, quantity } : entry));
+    };
+
+    @observable recent = [];
+    @action setRecent = recent => {
+        localStorage.recent = JSON.stringify(recent);
+        this.recent = recent;
+    };
+    cutRecent = () => this.recent.slice(-3); // 3 last products
+    addToRecent = id => this.setRecent([...this.cutRecent(), id]);
 
     @action initClientStore = () => runInAction(() => {
         this.favorite = localStorage.favorite ? JSON.parse(localStorage.favorite) : [];
         this.cart = localStorage.cart ? JSON.parse(localStorage.cart) : [];
+        this.recent = localStorage.recent ? JSON.parse(localStorage.recent) : [];
     });
 
     /*  Examples of store use.
@@ -64,8 +72,9 @@ class Store {
 
         this.favorite = data.favorite ?? [];
         this.cart = data.cart ?? [];
+        this.recent = data.recent ?? [];
 
-        /*  Examples of 
+        /*  Examples of hydrate use
 
             this.dark = !!data.dark;
 
