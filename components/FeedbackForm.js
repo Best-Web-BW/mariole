5f1aa36bdf0de9c893f12ca65d24e1f8a53c54e6
OcelotJungle <ocelotjungle@yandex.ru>
2021-03-31 20:00:02 +0500

    Minor feedback form changes

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,4 +1,4 @@
-import collectFormData from "../utils/common/collectFormData";
+import serializeForm from "../utils/common/serializeForm";
 import styles from "./FeedbackForm.module.scss";
 
 export default function FeedbackForm() {
@@ -6,28 +6,40 @@ export default function FeedbackForm() {
 		<div className={styles.form}>
             <form onSubmit={e => {
                 e.preventDefault();
-                console.log(collectFormData(e.target));
+                console.log(serializeForm(e.target));
             }}>
                 <div className={styles.form_row}>
                     <label className={styles.name}>
                         ИМЯ
-                        <input className={styles.input} type="text" name="name" autoComplete="name" />
+                        <input
+                            className={styles.input} type="text"
+                            name="name" autoComplete="given-name" required
+                        />
                     </label>
                     <label className={styles.email}>
                         EMAIL
-                        <input className={styles.input} type="text" name="email" autoComplete="email" />
+                        <input
+                            className={styles.input} type="email"
+                            name="email" autoComplete="email" required
+                        />
                     </label>
                 </div>
                 <div className={styles.form_row}>
                     <label className={styles.phone}>
                         ТЕЛЕФОН
-                        <input className={styles.input} type="text" name="phone" autoComplete="tel" />
+                        <input
+                            className={styles.input} type="tel"
+                            name="phone" autoComplete="tel" required
+                        />
                     </label>
                 </div>
                 <div className={styles.form_row}>
                     <label className={styles.comment}>
                         СООБЩЕНИЕ
-                        <textarea className={styles.input} type="text" name="message" autoComplete="off" />
+                        <textarea
+                            className={styles.input} type="text"
+                            name="message" autoComplete="off" required
+                        />
                     </label>
                 </div>
                 <div className={styles.form_row}>
