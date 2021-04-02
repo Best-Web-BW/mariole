c8eab8174c9109f1c2409c3e45b746c0aa65cf9e
OcelotJungle <ocelotjungle@yandex.ru>
2021-04-02 23:01:54 +0500

    Made auto refresh tokens util

---
+++ utils/auth/runAutoRefresh.js
@@ -0,0 +1,21 @@
+const refreshInterval = 1000 * 60 * 4 /* 4 minutes in ms */;
+
+export default function runAutoRefresh(store) {
+    runTimeout(store);
+}
+
+async function runTimeout(store) {
+    if(await refresh(store)) setTimeout(() => runTimeout(store), refreshInterval);
+}
+
+async function refresh(store) {
+    const response = await fetch("/api/auth/refresh");
+    console.log({ response });
+    if(response.status === 200) {
+        store.enableAdmin();
+        return true;
+    } else {
+        store.disableAdmin();
+        return false;
+    }
+}
