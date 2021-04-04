1668f47611a36d7ec2202030a78cd90b83864daa
OcelotJungle <ocelotjungle@yandex.ru>
2021-04-04 15:25:25 +0500

    Formatted user order email

--- pages/api/mail/order.js
+++ pages/api/mail/order.js
@@ -1,7 +1,6 @@
 import { methodNotAllowed } from "../../../utils/common/network";
 import { address, password } from "./data.json";
 import nodemailer from "nodemailer";
-const mariole = "Mario'le";
 
 export default function handler(req, res) {
     switch(req.method) {
@@ -10,6 +9,7 @@ export default function handler(req, res) {
     }
 }
 
+const mariole = "Mario’le Shop";
 const transporter = nodemailer.createTransport({
     host: "smtp.yandex.ru",
     port: 465,
@@ -17,114 +17,113 @@ const transporter = nodemailer.createTransport({
     auth: { user: address, pass: password }
 });
 
-function makeAdminMessage({ }) {
-    return {
-        from: `"${mariole}" <${address}>`,
-        to: address,
-        subject: `Заказ`,
-        html: `
-            <div>
-                <p>Информация о заказе №<span></span></p>
-                <p>Заказчик: <b></b></p>
-                <p>Телефон: <b></b></p>
-                <p>Email: <b></b></p>
-                <p>Адрес доставки: <span><i></i></span></p>
-                <br/>
-                <p>Товары в заказе:</p>
-                <ul>
-                    <li>
-                        <p>
-                            <span>name</span>
-                            <span>color</span>
-                            <span>size</span>
-                            <span>quantity</span>
-                            <span>price</span>
-                            <span>link</span>
-                        </p>
-                    </li>
-                </ul>
-                <br/>
-                <p>Метод оплаты: <span></span></p>
-                <p>Заказ на сумму: <b></b></p>
-                <p>Включая доставку: <b></b></p>
-            </div>
-        `
-    };
+const toAdminCartEntry = ({ href, name, color, size, quantity, price }) => (`
+    <li>
+        <p>
+            <span><a href="${href}">${name}</a></span>
+            <span>${color}</span>
+            <span>${size}</span>
+            <span>${quantity}</span>
+            <span>${price}</span>
+        </p>
+    </li>
+`);
+
+const makeAdminMessage = ({ id, name, phone, email, shippingAddress, cart, payment, totalPrice, shippingPrice }) => ({
+    from: `"${mariole}" <${address}>`,
+    to: address,
+    subject: `Заказ`,
+    html: `
+        <div>
+            <p>Информация о заказе №<span>${id}</span></p>
+            <p>Заказчик: <b>${name}</b></p>
+            <p>Телефон: <b>${phone}</b></p>
+            <p>Email: <b>${email}</b></p>
+            <p>Адрес доставки: <span><i>${shippingAddress}</i></span></p>
+            <br/>
+            <p>Товары в заказе:</p>
+            <ul>${cart.map(toAdminCartEntry).join("")}</ul>
+            <br/>
+            <p>Метод оплаты: <span>${payment}</span></p>
+            <p>Заказ на сумму: <b>${totalPrice}</b></p>
+            <p>Включая доставку: <b>${shippingPrice}</b></p>
+        </div>
+    `
+});
+
+const userStyles = {
+    row: "display: block;",
+    wrapper: `
+        border-bottom: 1px solid #cfcfcf;
+        display: flex; flex-wrap: nowrap;
+        justify-content: center;
+        align-items: center;
+    `,
+    image: "width: 20%; position: relative;",
+    quantity: `
+        position: absolute; top: 0; right: 0;
+        background-color: #cfcfcf;
+        border-radius: 50%; height: 20px;
+        width: 20px; font-size: 16px;
+        display: flex; flex-wrap: nowrap;
+        justify-content: center;
+        align-items: center;
+    `,
+    name: `
+        width: calc(40% - 20px);
+        padding-left: 20px;
+        font-size: 16px;
+    `,
+    price: `
+        width: 40%; display: flex;
+        flex-wrap: nowrap;
+        justify-content: flex-end;
+        font-size: 16px;
+    `
 }
 
-function makeUserMessage({ email }) {
-    return {
-        from: `"${mariole}" <${address}>`,
-        to: email,
-        subject: `Успешное оформление заказа`,
-        html: `
-        <div>
-        <h2>Спасибо за Ваш заказ!</h2>
-        <br/>
-        <p>Далее Вы можете ознакомиться с детальной информацией.</p>
-        <br/>
-        <p>Сумма вашего заказа: <b>summ</b></p>
-        <p>Включаяя доставку: <b></b></p>
-        <p>Метод оплаты: <b>наличными курьеру/картой курьеру/онлайн оплата</b></p>
-        <p>Доставить по адресу: <span>адрес</span></p>
-        <p>Заказчик: <b>Имя</b></p>
-        <br/>
-        <p>Вфш заказ:</p>
-        <div style="
-        display: block;
-        ">
-            <div style="
-            border-bottom: 1px solid #cfcfcf; 
-            display: flex;
-            flex-wrap: nowrap; 
-            justify-content: center;
-            align-items: center;
-            ">
-                <div className={styles.col_1} style="
-                width: 20%;
-                position: relative;
-                ">
-                    <img src="mario_le-1100 копия.jpg" alt="" width="100%" />
-                    <div className={styles.quantity} style="
-                    position: absolute;
-                    top: 0;
-                    right: 0;
-                    background-color: #cfcfcf;
-                    border-radius: 50%;
-                    height: 20px;
-                    width: 20px;
-                    font-size: 16px;
-                    display: flex;
-                    flex-wrap: nowrap; 
-                    justify-content: center;
-                    align-items: center;
-                    ">{ 1 }</div>
-                </div>
-                <div className={styles.col_2} style="
-                width: calc(40% - 20px);
-                padding-left: 20px;
-                font-size: 16px;
-                ">{ Название продукта } / { colors(color) }</div>
-                <div className={styles.col_3} style="
-                width: 40%;
-                display: flex;
-                flex-wrap: nowrap;
-                justify-content: flex-end;
-                font-size: 16px;
-                ">{ formatPrice(50000) } &#8381;</div>
+const toUserCartEntry = ({ image, quantity, name, color, price }) => (`
+    <div style="${userStyles.row}">
+        <div style="${userStyles.wrapper}">
+            <div style="${userStyles.image}">
+                <img src=${image} alt="" width="100%" />
+                <div style="${userStyles.quantity}">${quantity}</div>
             </div>
+            <div style="${userStyles.name}">${name} / ${color}</div>
+            <div style="${userStyles.price}">${formatPrice(price)} &#8381;</div>
         </div>
     </div>
-        `
-    };
-}
+`);
 
-async function _post({ email }) {
+const makeUserMessage = ({ email, totalPrice, shippingPrice, payment, shippingAddress, name, cart }) => ({
+    from: `"${mariole}" <${address}>`,
+    to: email,
+    subject: `Успешное оформление заказа`,
+    html: `
+        <div>
+            <h2>Спасибо за Ваш заказ!</h2>
+            <br />
+            <p>Далее Вы можете ознакомиться с детальной информацией.</p>
+            <br />
+            <p>Сумма вашего заказа: <b>${totalPrice}</b></p>
+            <p>Включая доставку: <b>${shippingPrice}</b></p>
+            <p>Метод оплаты: <b>${payment}</b></p>
+            <p>Доставить по адресу: <span>${shippingAddress}</span></p>
+            <p>Заказчик: <b>${name}</b></p>
+            <br />
+            <p>Ваш заказ:</p>
+            ${cart.map(toUserCartEntry).join("")}
+        </div>
+    `
+});
+
+async function _post({ id, name, phone, email, cart, payment, shippingAddress, shippingPrice, totalPrice }) {
     try {
-        const data = { email };
+        const adminData = { id, name, phone, email, cart, payment, shippingAddress, shippingPrice, totalPrice };
+        const userData = { name, email, cart, payment, shippingAddress, shippingPrice, totalPrice };
         await Promises.all([
-            transporter.sendMail(makeAdminMessage(data)),
-            transporter.sendMail(makeUserMessage(data))
+            transporter.sendMail(makeAdminMessage(adminData)),
+            transporter.sendMail(makeUserMessage(userData))
         ]);
         return { success: 1 };
     } catch(e) {
