621a8904d3135f01a69ab2c8d4cefda6ccb5bd20
OcelotJungle <ocelotjungle@yandex.ru>
2021-04-06 20:13:00 +0500

    Made admin's products API to work with DB

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,13 +1,16 @@
 import { success, error, methodNotAllowed } from "../../../utils/common/network";
 import leaveOneLocale from "../../../utils/products/leaveOneLocale";
 import connect from "../../../utils/mongo/connect";
+import { getMaxID } from "../../../utils/mongo/getMaxID";
+import { _get as authorize } from "../auth/authorize";
+import log from "../../../utils/mongo/log";
 
 export default async function handler(req, res) {
     switch(req.method) {
         case "GET": return await GET(req, res);
-        case "POST": return POST(req, res);
-        case "PATCH": return PATCH(req, res);
-        case "DELETE": return DELETE(req, res);
+        case "POST": return await POST(req, res);
+        case "PATCH": return await PATCH(req, res);
+        case "DELETE": return await DELETE(req, res);
         default: return methodNotAllowed(req, res, ["GET", "POST", "PATCH", "DELETE"]);
     }
 }
@@ -55,12 +58,44 @@ async function GET(req, res) {
     }
 }
 
-export function _post(data) {
-    console.log({ action: "_post", data });
-    return { success: 1 };
+export async function _post({ uuid, accessKey, parent: parentID, ...data }) {
+    let login;
+    try {
+        const authorization = await authorize({ uuid, accessKey });
+        if(authorization.success) login = authorization.login;
+        else return error("auth_error");
+    } catch(e) {
+        console.error(e);
+        return error("internal_error");
+    }
+
+    try {
+        const { db } = await connect();
+        const products = db.collection("products");
+
+        const parent = await products.findOne({ id: parentID }, { projection: { _id: 0, space: 1 } });
+        if(!parent) return error("no_parent");
+
+        const timestamp = new Date().toISOString();
+        data = {
+            id: await getMaxID(products) + 1,
+            cdate: timestamp,
+            mdate: timestamp,
+            space: parent.space,
+            ...data
+        };
+        await products.insertOne(data);
+        await log(login, "POST, product", data);
+    } catch(e) {
+        console.error(e);
+        return error("db_error");
+    }
+
+    return success();
 }
 
-function POST(req, res) {
+async function POST(req, res) {
+    const { uuid, access_key: accessKey } = req.cookies
     const {
         price, locales: {
             ru: {
@@ -75,12 +110,13 @@ function POST(req, res) {
                 composition: enComposition,
                 details: enDetails
             }
-        }, color, sizes, /* images, */
+        }, color, sizes, images,
         category, subcategory,
         limited, bestseller,
         available, parent
     } = req.body;
-    const result = _post({
+    const result = await _post({
+        uuid, accessKey,
         price, locales: {
             ru: {
                 name: ruName,
@@ -94,20 +130,57 @@ function POST(req, res) {
                 composition: enComposition,
                 details: enDetails
             }
-        }, color, sizes, /* images, */
+        }, color, sizes, images,
         category, subcategory,
         limited, bestseller,
         available, parent
     });
-    res.status(200).json(result);
+    if(result.success) res.status(200).end();
+    else switch(result.error) {
+        case "auth_error":
+            res.status(403).end();
+            break;
+        default:
+            res.status(500).end(result.error);
+            break;
+    }
 }
 
-export function _patch(data) {
-    console.log({ action: "_patch", data });
-    return { success: 1 };
+export async function _patch({ uuid, accessKey, ...data }) {
+    let login;
+    try {
+        const authorization = await authorize({ uuid, accessKey });
+        if(authorization.success) login = authorization.login;
+        else return error("auth_error");
+    } catch(e) {
+        console.error(e);
+        return error("internal_error");
+    }
+
+    try {
+        const { db } = await connect();
+        const products = db.collection("products");
+
+        const product = await products.findOne({ id: data.id });
+        if(!product) return error("no_product");
+
+        data = {
+            ...product,
+            ...data,
+            mdate: new Date().toISOString()
+        };
+        await products.updateOne({ _id: product._id }, { $set: data });
+        await log(login, "PATCH, product", data);
+    } catch(e) {
+        console.error(e);
+        return error("db_error");
+    }
+
+    return success();
 }
 
-function PATCH(req, res) {
+async function PATCH(req, res) {
+    const { uuid, access_key: accessKey } = req.cookies;
     const { id } = req.query;
     const {
         price, locales: {
@@ -123,12 +196,13 @@ function PATCH(req, res) {
                 composition: enComposition,
                 details: enDetails
             }
-        }, color, sizes, /* images, */
+        }, color, sizes, images,
         category, subcategory,
         limited, bestseller,
         available
     } = req.body;
-    const result = _patch({
+    const result = await _patch({
+        uuid, accessKey,
         id: +id, price, locales: {
             ru: {
                 name: ruName,
@@ -142,21 +216,61 @@ function PATCH(req, res) {
                 composition: enComposition,
                 details: enDetails
             }
-        }, color, sizes, /* images, */
+        }, color, sizes, images,
         category, subcategory,
         limited, bestseller,
         available
     });
-    res.status(200).json(result);
+    if(result.success) res.status(200).end();
+    else switch(result.error) {
+        case "auth_error":
+            res.status(403).end();
+            break;
+        default:
+            res.status(500).end(result.error);
+            break;
+    }
 }
 
-function _delete({ id }) {
-    console.log({ action: "_delete", id });
-    return { success: 1 };
+export async function _delete({ uuid, accessKey, id }) {
+    let login;
+    try {
+        const authorization = await authorize({ uuid, accessKey });
+        if(authorization.success) login = authorization.login;
+        else return error("auth_error");
+    } catch(e) {
+        console.error(e);
+        return error("internal_error");
+    }
+
+    try {
+        const { db } = await connect();
+        const products = db.collection("products");
+
+        const product = await products.findOne({ id });
+        if(!product) return error("no_product");
+
+        await products.deleteOne({ _id: product._id });
+        await log(login, "DELETE, product", data);
+    } catch(e) {
+        console.error(e);
+        return error("db_error");
+    }
+
+    return success();
 }
 
-function DELETE(req, res) {
+async function DELETE(req, res) {
+    const { uuid, access_key: accessKey } = req.cookies;
     const { id } = req.query;
-    const result = _delete({ id: +id });
-    res.status(200).json(result);
+    const result = await _delete({ uuid, accessKey, id: +id });
+    if(result.success) res.status(200).end();
+    else switch(result.error) {
+        case "auth_error":
+            res.status(403).end();
+            break;
+        default:
+            res.status(500).end(result.error);
+            break;
+    }
 }
