a05beeb80a69449fce6f7057d2e4e0d079d1b323
OcelotJungle <ocelotjungle@yandex.ru>
2021-04-07 09:44:00 +0500

    Added translation for shipping

--- pages/shipping/index.js
+++ pages/shipping/index.js
@@ -2,8 +2,8 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import blocks from "../../scss/blocks.module.scss";
 import { useTranslation } from "next-i18next";
 import styles from "./index.module.scss";
-import cn from "classnames";
 import Head from "next/head";
+import cn from "classnames";
 
 export const getStaticProps = async ({ locale }) => ({
     props: { ...await serverSideTranslations(locale, ["page_shipping"]) }
@@ -17,52 +17,25 @@ export default function Shipping() {
             <title>{ t("title") }</title>
         </Head>
         <div className={cn(blocks.content_block, styles.page)}>
-            <h2 className={styles.page_title}>ПОЛИТИКА ДОСТАВКИ</h2>
+            <h2 className={styles.page_title}>{ t("delivery-policy") }</h2>
             <div className={styles.attention}>
-                <Data>
-                    Просим обратить внимание, что в связи с ситуацией в мире 
-                    из-за пандемии транспортные компании работают с повышенной 
-                    нагрузкой. Сроки доставки и возврата могут быть увеличены.
-                </Data>
+                <Data>{ t("data1") }</Data>
             </div>
+            <Data>{ t("data2") }</Data>
+            <Data>{ t("data3") }</Data>
+            <Data>{ t("data4") }</Data>
+            <Data>{ t("data5") }</Data>
+            <Data>{ t("data6") }</Data>
+            <Data>{ t("data7") }</Data>
+            <Data>{ t("data8") }</Data>
+            <Data>{ t("data9") }</Data>
             <Data>
-                Доставка осуществляется с понедельника по пятницу, 
-                с 9:00 до 18:00. Клиентам из России и Казахстана мы 
-                предлагаем доставку курьерской службой CDEK. 
-                У покупателей из этих стран есть возможность получить 
-                заказ в офисе CDEK или перенести дату доставки.
-            </Data>
-            <Data>
-                В других странах заказ может быть доставлен другими 
-                курьерскими службами, включая UPS, FedEx, TNT и KCE.
-            </Data>
-            <Data>
-                Доставка формируется и передаётся курьерской службе 
-                в течение 2 рабочих дней с момента оформления заказа.
-            </Data>
-            <Data>При возврате заказа, стоимость доставки не возвращается.</Data>
-            <Data>
-                Сроки доставки устанавливаются курьерской службой 
-                и зависят от страны и региона доставки.
-            </Data>
-            <Data>
-                Таможенные сборы не включены в стоимость товара 
-                при доставке в другие страны и регионы.
-            </Data>
-            <Data>Услуга «примерка» не предоставляется при доставке.</Data>
-            <Data>
-                Обратите внимание, что налоги и таможенные сборы не 
-                включены в стоимость товара. Величина сборов зависит 
-                от таможенных регламентов страны доставки. Ответственность 
-                за оплату налогов и сборов лежит на покупателе.
-            </Data>
-            <Data>
-                Если у вас остались вопросы, пожалуйста, свяжитесь 
-                с нами в Whatsapp по номеру телефона:&nbsp;
-                <a href="tel:+7(968)001-88-80" className={styles.link}>+7 (968) 001-88-80</a>
-                &nbsp;или e-mail:&nbsp;
-                <a href="mailto:mariolewool@yandex.ru" className={styles.link}>Mariolewool@yandex.ru</a>
-                , и мы с радостью вам поможем.
+                { t("data10-1") }
+                &nbsp;<a className={styles.link} href="tel:+79680018880">+7 (968) 001-88-80</a>,
+                &nbsp;<a className={styles.link} href="tel:+79295005146">+7 (929) 500-51-46</a>
+                &nbsp;{ t("data10-2") }
+                &nbsp;<a className={styles.link} href="mailto:mariolewool@yandex.ru">Mariolewool@yandex.ru</a>
+                &nbsp;{ t("data10-3") }
             </Data>
         </div>
     </>);
