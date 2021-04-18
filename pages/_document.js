430dc183e47f07683ff95a64ecc32f5f9daeba4f
OcelotJungle <ocelotjungle@yandex.ru>
2021-04-18 22:22:09 +0500

    Changed icons

--- pages/_document.js
+++ pages/_document.js
@@ -10,28 +10,30 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/* <link rel="stylesheet" href="/fonts/Fontello/css/fontello.css" />
-                    <link rel="stylesheet" href="/fonts/Fontello/css/animation.css" />
-                    <link rel="stylesheet" href="/fonts/Fontello/css/fontello-codes.css" />
-                    <link rel="stylesheet" href="/fonts/Fontello/css/fontello-embedded.css" /> */}
-
+                    <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-touch-icon-57x57.png" />
+                    <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png" />
+                    <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-touch-icon-72x72.png" />
+                    <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png" />
+                    <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-touch-icon-114x114.png" />
                     <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon-120x120.png" />
+                    <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-touch-icon-144x144.png" />
                     <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
-                    <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png" />
-                    <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-                    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#000000" />
-                    <meta name="msapplication-TileImage" content="/icons/mstile-144x144.png" />
-                    <link rel="shortcut icon" href="/icons/favicon.ico" />
 
+                    <link rel="shortcut icon" href="/icons/favicon.ico" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="194x194" href="/icons/favicon-194x194.png" />
+                    <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-chrome-192x192.png" />
+                    
+                    <meta name="msapplication-TileImage" content="/icons/mstile-144x144.png" />
+                    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#9c877c" />
+                    
+                    <meta name="theme-color" content="#ffffff" />
+                    <meta name="msapplication-TileColor" content="#ffffff" />
+                    
                     <link rel="manifest" href="/icons/manifest.json" />
                     <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-                    <meta name="apple-mobile-web-app-title" content="Mario'le Shop" />
-                    <meta name="application-name" content="Mario'le Shop" />
-                    <meta name="msapplication-TileColor" content="#ffffff" />
-                    <meta name="theme-color" content="#ffffff" />
                 </Head>
                 <body>
                     <Main />
