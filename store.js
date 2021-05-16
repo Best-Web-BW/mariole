73ea8ff06c1e2e0cb28b3188b373c8e341e6aada
OcelotJungle <ocelotjungle@yandex.ru>
2021-05-16 11:14:28 +0500

    Added uid with format "id-size" for identification products in cart

--- store.js
+++ store.js
@@ -27,11 +27,19 @@ class Store {
         localStorage.setItem("cart", JSON.stringify(cart));
         this.cart = cart;
     };
-    filterCart = id => this.cart.filter(entry => entry.id !== id);
-    addToCart = (id, size, quantity) => this.setCart([...this.filterCart(id), { id, size, quantity }]);
-    removeFromCart = id => this.setCart(this.filterCart(id));
-    setCartQuantity = (id, quantity) => {
-        this.setCart(this.cart.map(entry => entry.id === id ? { ...entry, quantity } : entry));
+    filterCartID = id => this.cart.filter(entry => entry.id !== id);
+    filterCartUID = uid => this.cart.filter(entry => entry.uid !== uid);
+    addToCart = (id, size, quantity) => {
+        const uid = `${id}-${size}`;
+        this.setCart([...this.filterCartUID(uid), { uid, id, size, quantity }]);
+    };
+    removeFromCart = (id, size) => {
+        if(!size) this.setCart(this.filterCartID(id));
+        else this.setCart(this.filterCartUID(`${id}-${size}`));
+    };
+    setCartQuantity = (id, size, quantity) => {
+        const uid = `${id}-${size}`;
+        this.setCart(this.cart.map(entry => entry.uid === uid ? { ...entry, quantity } : entry));
     };
     resetCart = () => this.setCart([]);
 
